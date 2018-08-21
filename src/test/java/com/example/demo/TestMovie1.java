d0e2f67d428366c2c0e2f7715f448637942c6d4f
Mona Lisa <josemyjoseph@gmail.com>
2018-08-21 11:56:22 -0400

    fixed test suite

--- src/test/java/com/example/demo/TestMovie1.java
+++ src/test/java/com/example/demo/TestMovie1.java
@@ -1,13 +1,19 @@
 package com.example.demo;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.HashSet;
 import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
-
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@AutoConfigureTestDatabase(replace=AutoConfigureTestDatabase.Replace.NONE)
 public class TestMovie1 {
 
   @Autowired
@@ -19,16 +25,18 @@ public class TestMovie1 {
     actor.setName("Sandra Bullock");
     actor.setRealname("Sandra Mae Bullowski");
 
-//    Movie movie = new Movie();
-//    movie.setTitle("Emoji Movie");
-//    movie.setYear(2017);
-//    movie.setDescription("About Emojis...");
+    Movie movie = new Movie();
+    movie.setTitle("Emoji Movie");
+    movie.setYear(2017);
+    movie.setDescription("About Emojis...");
 
-//    Set<Movie> movies = new HashSet<Movie>();
-//    movies.add(movie);
+    Set<Movie> movies = new HashSet<Movie>();
+    movies.add(movie);
 
-//    actor.setMovies(movies);
+    actor.setMovies(movies);
     actorRepository.save(actor);
     assertEquals(actorRepository.findById(1L).get().getId(), actor.getId());
+
+//    assertEquals(actor.getName(), "Sandra Bullock");
   }
 }
--- src/test/java/com/example/demo/TestMovie2.java
+++ src/test/java/com/example/demo/TestMovie2.java
@@ -1,33 +1,41 @@
 package com.example.demo;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.HashSet;
 import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
-
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@AutoConfigureTestDatabase(replace=AutoConfigureTestDatabase.Replace.NONE)
 public class TestMovie2 {
+
   @Autowired
   ActorRepository actorRepository;
   @Test
   public void testActor1() {
-    Actor actor = new Actor();
-    actor.setName("David Bullock");
-    actor.setRealname("David Mae Bullowski");
+    Actor actor1 = new Actor();
+    actor1.setName("David Bullock");
+    actor1.setRealname("David Mae Bullowski");
 
-//    Movie movie = new Movie();
-//    movie.setTitle("Some Movie");
-//    movie.setYear(2019);
-//    movie.setDescription("About some movie...");
-//
-//    Set<Movie> movies = new HashSet<Movie>();
-//    movies.add(movie);
-//
-//    actor.setMovies(movies);
-    actorRepository.save(actor);
+    Movie movie1 = new Movie();
+    movie1.setTitle("Some Movie");
+    movie1.setYear(2019);
+    movie1.setDescription("About some movie...");
 
-    assertEquals(actorRepository.findById(1L).get().getId(), actor.getId());
+    Set<Movie> movies1 = new HashSet<Movie>();
+    movies1.add(movie1);
+
+    actor1.setMovies(movies1);
+    actorRepository.save(actor1);
+
+    assertEquals(actorRepository.findById(2L).get().getId(), actor1.getId());
+//    assertEquals(actor1.getRealname(),  "David Mae Bullowski");
   }
 }
